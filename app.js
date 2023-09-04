f2923f49580f375f3d17066c2e5b4413e3cccbca
francoromeo <francoromeo5012@gmail.com>
2023-09-03 22:10:59 -0300

    js messages and notification

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const menuItems = document.querySelectorAll('.menu-item');
 
+const messagesNotification = document.querySelector('#messages-notification');
+const messages = document.querySelector('.messages');
+
 const changeActiveItem = () => {
     menuItems.forEach(item => {
         item.classList.remove('active');
@@ -17,4 +20,12 @@ menuItems.forEach(item => {
             document.querySelector('#notifications .notification-count').style.display = 'none';
         }
     })
+})
+
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
 })
