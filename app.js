3d2f528821c8801a5808633f7cd3d91e9474d74d
francoromeo <francoromeo5012@gmail.com>
2023-09-04 00:45:39 -0300

    change color

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 const root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
 
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -99,5 +100,33 @@ fontSizes.forEach(size => {
         }
     
         document.querySelector('html').style.fontSize = fontSize;
+    })
+})
+
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        let primary;
+        changeActiveColorClass();
+
+        if(color.classList.contains('color-1')){
+            primaryHue = 252;
+        } else if (color.classList.contains('color-2')){
+            primaryHue = 52;
+        } else if (color.classList.contains('color-3')){
+            primaryHue = 352;
+        } else if (color.classList.contains('color-4')){
+            primaryHue = 152;
+        } else if (color.classList.contains('color-5')){
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+
+        root.style.setProperty('--primary-color-hue', primaryHue);
     })
 })
